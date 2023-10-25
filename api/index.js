39a1ef389815efe04be3c0d9f0d7dbbfaf0158e9
freire-guido <14301656+freire-guido@users.noreply.github.com>
2023-10-25 19:05:28 -0300

    materias.json

--- api/index.js
+++ api/index.js
@@ -2,12 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-const materias = [
-  { id: 1, nombre: 'Intro. Bases de Datos', cht: 64 },
-  { id: 2, nombre: 'Aprendizaje Automático', cht: 64 },
-  { id: 3, nombre: 'Organización del Computador I', cht: 96 },
-  { id: 4, nombre: 'Lógica y Computabilidad', cht: 112 }
-]
+const materias = require('./materias.json')
 
 app.get('/api/materias/buscar', (req, res) => {
   const busqueda = req.query.q;
