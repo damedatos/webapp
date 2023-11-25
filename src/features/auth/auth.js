ad5ae9e3d8a66d0a10044c576f9070343a50aa43
freire-guido <14301656+freire-guido@users.noreply.github.com>
2023-11-25 14:13:20 -0300

    auth recordame

--- src/features/auth/auth.js
+++ src/features/auth/auth.js
@@ -1,15 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { authenticate } from './authSlice'
 
 export function Auth() {
     const dispatch = useDispatch()
     const [hash, setHash] = useState(null)
+    const [recordame, setRecordame] = useState(false)
+    function handleRecordar(e) {
+        setRecordame(e.target.checked)
+    }
     function handleInput(e) {
         const input = e.target.value
         setHash(input)
     }
     function handleEntrar() {
+        if (recordame) {
+            localStorage.setItem("hash", hash)
+        }
         dispatch(authenticate(hash))
     }
     async function handleNoHash() {
@@ -18,15 +25,21 @@ export function Auth() {
             // .catch(err => TODO)
         dispatch(authenticate(res.ip))
     }
+    useEffect(() => {
+        const hashGuardado = localStorage.getItem("hash")
+        if (hashGuardado) {
+            dispatch(authenticate(hashGuardado))
+        }
+    }, [])
     return(
         <div className='d-flex flex-column align-items-center vh-100'>
             <form className='m-auto'>
                 <img src='dD.png' width='100' height='100'></img>
                 <h3>betaTest</h3>
                 <h1 className = 'mb-3'>Iniciar sesion</h1>
-                <input className='form-control mb-3' onChange = {handleInput} placeholder='Hash'></input>
+                <input className='form-control mb-3' onChange = {handleInput} placeholder='Hash' id = 'hashInput'></input>
                 <div className='form-check text-start my-3'>
-                    <input className='form-check-input' type='checkbox'></input>
+                    <input className='form-check-input' type='checkbox' onChange = {handleRecordar}></input>
                     <label className='form-check-label'>Recordame</label>
                 </div>
                 <div className='d-grid gap-2'>
