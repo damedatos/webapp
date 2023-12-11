6e5830b426d416e37bccb5d4acbbf54ad81c1488
freire-guido <14301656+freire-guido@users.noreply.github.com>
2023-12-11 19:38:55 -0300

    daisyui navbar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,28 @@
+import '../styles/globals.css';
 import Head from 'next/head'
 import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
     return (<>
-        <Head>
-            <title>dameDatos</title>
-        </Head>
-        <div className="nav">
-            <Link href="/">
-                <img id="title" src="dD.png" width ="33" height="33"/>
-            </Link>
+        <Head><title>dameDatos</title></Head>
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <a className="btn btn-ghost text-xl">dameDatos</a>
+            </div>
+            <div className="flex-none gap-2">
+                <div className="form-control">
+                    <input type="text" placeholder="Buscar" className="input input-bordered w-25 me-3" />
+                </div>
+                <div tabIndex={0} role="button" className="dropdown dropdown-end">
+                    <span className="btn btn-ghost text-xl">gF</span>
+                    <ul tabIndex={0} className="mt-5 p-3 gap-3 shadow menu dropdown-content rounded-box w-20">
+                        <li>Perfil</li>
+                        <li>Salir</li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div className="wrapper grid">
         <Component {...pageProps} />
-        </div>
     </>)
 }
 
--- src/pages/api/materias/[nombre].js
+++ src/pages/api/materias/[nombre].js
@@ -1,12 +1,20 @@
 import dbConnect from '../../../lib/dbConnect'
 import Materia from '../../../models/materia'
-
 export default async function handler(req, res) {
     const { nombre } = req.query
     await dbConnect()
     try {
-        const materia = await Materia.find({nombre: nombre})
-        res.status(200).json({ success: true, data: materia })
+        // const materias = await Materia.find({nombre: nombre})
+        const materias = await Materia.aggregate([
+            {$search: {
+                index: "nombre",
+                text: {
+                    query: nombre,
+                    path: "nombre"
+                }
+            }}
+        ])
+        res.status(200).json({ success: true, data: materias })
     } catch (e) {
         res.status(400).json({ success: false })
     }
